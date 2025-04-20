e7839eeb2cde8afe91e2bff336e0112bbaeccf5f
veiledcoder <zunashkhan313@gmail.com>
2025-04-20 21:40:11 +0500

    hi

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,24 +5,25 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col-reverse md:flex-row items-center justify-center px-4 md:px-12 py-4 md:py-10 gap-0 md:gap-8"
+      className="min-h-screen flex flex-col-reverse md:flex-row items-center justify-center px-4 md:px-12 py-2 md:py-10 gap-0 md:gap-8"
+
     >
       {/* Left Side - Text */}
       <motion.div
-        className="flex-1 text-center md:text-left"
+        className="flex-1 text-center md:text-left space-y-2"
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <h1 className="text-4xl sm:text-3xl lg:text-5xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-tight mb-2">
+        <h1 className="text-4xl sm:text-3xl lg:text-5xl font-bold bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-tight">
           Hi, I'm Syed Huzafa Tassawer
         </h1>
 
-        <p className="text-gray-500 text-base sm:text-lg md:text-xl max-w-xl mx-auto md:mx-0 mb-4">
+        <p className="text-gray-500 text-base sm:text-lg md:text-xl max-w-xl mx-auto md:mx-0">
           I’m a passionate digital marketing specialist with a unique blend of Islamic academic background and modern media expertise.
         </p>
 
-        <div className="flex justify-center md:justify-start space-x-4 mb-4">
+        <div className="flex justify-center md:justify-start space-x-4">
           <a
             href="#projects"
             className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition hover:-translate-y-0.5 hover:shadow-lg"
@@ -75,7 +76,7 @@ export const Home = () => {
 
       {/* Right Side - Image */}
       <motion.div
-        className="flex-1 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg md:pt-0"
+        className="flex-1 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg  md:pt-0"
         initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
@@ -83,7 +84,7 @@ export const Home = () => {
         <img
           src="./huzafa.jpg"
           alt="Syed Huzafa Tassawer"
-          className="w-40 h-40 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 rounded-2xl shadow-xl hover:shadow-2xl transition duration-500 hover:scale-105 object-cover object-center mx-auto"
+          className="w-40 h-40 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 rounded-2xl shadow-xl hover:shadow-2xl transition duration-500 hover:scale-105 object-cover object-center"
         />
       </motion.div>
     </section>
