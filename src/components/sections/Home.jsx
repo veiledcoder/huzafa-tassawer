bd22eb7518155a36e33541d99c59e8cdeaca6fef
veiledcoder <zunashkhan313@gmail.com>
2025-04-20 21:58:18 +0500

    ok

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
     >
       {/* Left Side - Text */}
       <motion.div
-        className="flex-1 text-center md:text-left space-y-2"
+        className="flex-1 text-center md:text-left pt-12 space-y-2"
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
@@ -75,7 +75,7 @@ export const Home = () => {
 
       {/* Right Side - Image */}
       <motion.div
-        className="flex-1 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg pt-12 pb-0 mb-0 gap-y-0 md:pt-0"
+        className="flex-1 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg justify-center items-center pt-12 pb-0 mb-0 gap-y-0 md:pt-0"
         initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
